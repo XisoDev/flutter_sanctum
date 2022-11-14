4184c65350b6011d67d0082bbac2f28d395128a4
xiso <xiso@amuz.co.kr>
2022-11-14 19:53:51 +0900

    first

---
+++ android/app/src/main/kotlin/kr/amuz/sanctum/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.amuz.sanctum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,1 @@
+const API_URL = 'https://mycup.lge.amuz.kr'; // Put your API URL here, e.g. https://abcd1234.ngrok.io/api
